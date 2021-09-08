40648343e3625670ca5721834bc787d4ce1f10f5
RohanMondal <>
2021-09-08 17:30:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// var str="Hello";
+// console.log(str);
+// var readlinesync=require("readline-sync");
+// var name=readlinesync.question('may i have your name?');
+// console.log('your name is:'+name);
+function hello(var1,var2){
+  return var1+var2;
+}
+console.log(hello(3,"rohan",2));
